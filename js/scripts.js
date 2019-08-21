66f4fb984e5d06593b2013c64c5149952a5d00f5
Ali Amirhamzeh, Alan Hermanns and Christine Frank <christine@>
2019-08-21 10:23:29 -0700

    add function to handle single word vowels

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,12 @@ $(document).ready(function(){
 });
 
 function toPigLatin(userString){
+  var vowels =["a", "e", "i", "o", "u"];
+  if (vowels.includes(userString.toLowerCase())){
+    userString += "way";
+  }
+
+
+
  return userString;
 }
